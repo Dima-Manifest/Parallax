0c6590f8b1cb3bd7e2f2e4761a2633d63da1e9f1
Dima-Manifest <medvedeva.sai82@gmail.com>
2025-04-22 21:27:43 +0500

    paralaxs

--- js/main.js
+++ js/main.js
@@ -5,11 +5,11 @@ let train = document.getElementById('train');
 let man = document.getElementById('man-out');
 let desert_moon = document.getElementById('desert_moon');
 
-window.addEventListener('scroll', () =>{ 
+window.addEventListener('scroll', () => { 
   let value = window.scrollY;
-  moon.style.top = value *0.9 + 'px';
+  moon.style.top = value * 0.9 + 'px';
   text.style.top = 80 + value * -0.13 + '%';
   train.style.left =  value * 1.2 + 'px';
   man.style.left =  value * 0.7 + 'px';
-  desert_moon.style.top = value * 0.3 + 'px'
+  desert_moon.style.top = value * 0.3 + 'px';
 })
